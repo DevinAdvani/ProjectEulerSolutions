6916e058b58d819f969910a9d7696ebc31b05624
ColderThanSeptember <80278580+DevinAdvani@users.noreply.github.com>
2022-12-29 14:34:44 +0000

    Main

--- Solutions/108.py
+++ Solutions/108.py
@@ -5,25 +5,47 @@ pi (2a +1) - 1/ 2 +1
 def arraytosolutions(arrayofpowers):
     n = 1
     for i in range(0,len(arrayofpowers)):
-        n *= arrayofpowers[i]
-        return (n-1)/2 + 1
+        n *= ((2 * arrayofpowers[i])+1)
+    return (n-1)/2 + 1
     
 def numtoarray(num):
-    n = 1
-    index = 0
+    primeindex = -1
+    index = -1
     powerarray = []
     while num != 1:
-        n += 1
-        while num % n:
-            num /= n
+        primeindex += 1
+        if num % primes[primeindex] == 0:
+            index += 1
+            powerarray.append(0)
+        while num % primes[primeindex] == 0:
+            num /= primes[primeindex]
+            powerarray[index] += 1
+    return powerarray
 
-maxsolutionnum = 0
-n = 1
-while maxsolutionnum < 1000:
-    n += 1
-    solutions = arraytosolutions(numtoarray(n))
-    if solutions > maxsolutionnum:
-        maxsolutionnum = solutions
-        print(n,maxsolutionnum)
-        
-print(n,maxsolutionnum)
+def f(a):
+    return arraytosolutions(numtoarray(a))
+
+def primes_sieve2(limit):
+    a = [True] * limit                          # Initialize the primality list
+    a[0] = a[1] = False
+
+    for (i, isprime) in enumerate(a):
+        if isprime:
+            yield i
+            for n in range(i*i, limit, i):     # Mark factors non-prime
+                a[n] = False
+              
+    return a
+                
+
+primes = list(primes_sieve2(1000000))
+
+
+int = 1
+maxsol = 0
+while maxsol < 1000:
+    int += 1
+    sol = f(int)
+    if sol > maxsol:
+        maxsol = sol 
+        print(int,maxsol)
--- speedtest.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,1000000):
-    print(i)
-    
-# in 20 seconds
